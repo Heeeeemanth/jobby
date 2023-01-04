dd2fb2e56b4fd0396406203dc32f1c11312787b9
Heeeeemanth <ahemanthkumar27@gmail.com>
2023-01-04 11:09:40 +0530

    #jobs page pending

--- src/Components/LoginPage/login.js
+++ src/Components/LoginPage/login.js
@@ -5,9 +5,18 @@ const LoginPage = () => {
 
     return <>
 <div className='bg-container'>
-<div className='inner-login-container'>
-<img src='https://assets.ccbp.in/frontend/react-js/logo-img.png' alt='https://assets.ccbp.in/frontend/react-js/logo-img.png'/>
+<form className='inner-login-container' >
+<img className='loginpagelogo' src='https://assets.ccbp.in/frontend/react-js/logo-img.png' alt='https://assets.ccbp.in/frontend/react-js/logo-img.png'/>
+<div className='username'>
+    <label className='userlabel' for='username'>USERNAME</label>
+    <input className='username-bar' id='username' type='text' placeholder='Username'/>
 </div>
+<div className='password'>
+    <label className='passwordlabel' for='password'>PASSWORD</label>
+    <input className='Passsword-bar' id='password' type='password' placeholder='Password'/>
+</div>
+<button className='loginBtn'>Login</button>
+</form>
 </div>
     </>
 }
